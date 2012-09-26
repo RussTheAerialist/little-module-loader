808093b9859b8f27d5b0554f04cf70efa18c6747
Russell Hay <russell.hay@gmail.com>
2012-09-25 21:26:48 -0700

    Removing namespace package since they appear to have problems in virtualenv

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup
 setup(
     name = "little.module_loader",
     version = "0.1",
-    packages = [ 'little.module_loader' ],
-    namespace_packages = [ 'little' ],
-    
+    packages = [ 'little', 'little.module_loader' ],
+
     author = "Russell Hay",
     author_email = "me@russellhay.com",
     description = "A very simple module loader package for dynamically creating lists of modules, classes, and functions",
     license = "PSF",
-    
+
     tests_require = [ "nose" ],
-    test_suite = "tests", 
+    test_suite = "tests",
+    use_2to3 = True,
 )
